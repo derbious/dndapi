c9df7c5e75a3daa40534d0581c1971b66a29d8b1
Steven Anthony <steven@derbious.com>
2019-10-10 23:10:49 -0400

    Getting it working

--- src/dndapi/static/js/newdndonations.js
+++ src/dndapi/static/js/newdndonations.js
@@ -1,5 +1,21 @@
-function SearchPage($scope){
+var dndApp = angular.module('dnd',[]);
 
-  
-
-}
+dndApp.controller('LoginController', ['$scope', '$http', function($scope, $http) {
+    $scope.apikey = "";
+    $scope.doLogin = function(user){
+        console.log('Doing Login AJAX shit');
+        $http({
+            method: 'POST',
+            url: 'api/auth',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: user
+        }).then(function successCallback(response) {
+            console.log('Success /api/auth');
+            $scope.apikey = response;
+        }, function errorCallback(response) {
+            $scope.apikey="ERROR";
+        });
+    }
+}]);
